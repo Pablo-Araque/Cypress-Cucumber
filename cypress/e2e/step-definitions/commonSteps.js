9bd6e95a30cb9ef04f35d89c0b9aee7636bc2d98
Malomalote <paraqueserichol@gmail.com>
2025-03-23 12:36:19 +0100

    ejercicios desde 0

--- cypress/e2e/step-definitions/commonSteps.js
+++ cypress/e2e/step-definitions/commonSteps.js
@@ -7,5 +7,23 @@ import { CommonPage} from "../pages/commonPage"
 let commonPage = new CommonPage();
 
 Given("I visit {string}", (url) => {
-    commonPage.visitLink(url);
-    });
+  commonPage.visitLink(url);
+ });
+
+ Given("I check that the url doesn't include the endpoint {string}", (endpoint) => {
+  commonPage.checkUrlnotInclude(endpoint);
+ });
+
+ Given("I check that the url include the endpoint {string}", (endpoint) => {
+  commonPage.checkUrlInclude(endpoint);
+ });
+ 
+ Given("I click on the button with data-test {string}", (buttonByDataTest) => {
+  commonPage.clickButtonByDataTest(buttonByDataTest);
+ });
+
+ Given("I type in the text box with data test {string} the text {string}", (texBoxByDataText, text) => {
+  commonPage.typeInTextBoxByDataTest(texBoxByDataText, text);
+ });
+ 
+
