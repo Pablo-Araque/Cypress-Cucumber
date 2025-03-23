6e9a3eb12959efd54a3eede26e6c3778c0d17a00
Malomalote <paraqueserichol@gmail.com>
2025-03-23 14:36:47 +0100

    ejercicios 1

--- cypress/e2e/basic-tests/basic.js
+++ cypress/e2e/basic-tests/basic.js
@@ -15,4 +15,48 @@ describe("basic test", () => {
     cy.get('[data-test="primary-header"]').should('contain', 'Swag Labs');
     });
 
+ 
+    it("all Accepted usernames be visible", () => {
+      cy.get('[data-test="login-credentials-container"]').find('[data-test="login-credentials"]')
+      const users = [
+        'standard_user',
+        'locked_out_user',
+        'problem_user',
+        'performance_glitch_user',
+        'error_user',
+        'visual_user'
+      ];
+      users.forEach((user) => {
+        cy.contains(user).should('be.visible');
+      });
+     });
+  
+
+     it("login error username", () => {
+      cy.get('[data-test="login-button"]').click()
+      cy.get('.error-message-container').find('[data-test="error"]').should('contain', 'Epic sadface: Username is required')
+      });
+
+
+      it("login error password", () => {
+        cy.get('[data-test="username"]').type('standard_user');
+        cy.get('[data-test="login-button"]').click();
+        cy.get('.error-message-container').find('[data-test="error"]').should('contain', 'Epic sadface: Password is required')
+        });
+
+
+        it("login error username and password", () => {
+          cy.get('[data-test="username"]').type('standard');
+          cy.get('[data-test="password"]').type('secret');
+          cy.get('[data-test="login-button"]').click();
+          cy.get('.error-message-container').find('[data-test="error"]').should('contain','Epic sadface: Username and password do not match any user in this service')
+          });
+
+
+          it("login error lock user", () => {
+            cy.get('[data-test="username"]').type('locked_out_user');
+            cy.get('[data-test="password"]').type('secret_sauce');
+            cy.get('[data-test="login-button"]').click();
+            cy.get('.error-message-container').find('[data-test="error"]').should('contain','Epic sadface: Sorry, this user has been locked out.')
+            });
 })
